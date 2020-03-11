6d5cc2df9335225844c2529d4a6c8eea712a3bec
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-11 10:15:58 +0530

    ADDED : game with two player

--- snakeAndladder.sh
+++ snakeAndladder.sh
@@ -3,50 +3,99 @@
 echo "WelCome to Snake and Ladder Computation"
 
 #CONSTANT VARIABLE
-PlayerCurrentPosition=0
+PlayerOnePosition=0
+PlayerTwoPosition=0
 WinningPosition=100
-diceCount=0
+diceCount1=0
+diceCount2=0
+player=1
+
+function playerTurn()
+	{
+		if [ $player == 1 ]
+		then
+			player=2
+		else
+			player=1
+		fi
+		echo $player
+	}
 
 function rollDice()
 	{
 		dice=$((1+RANDOM%6))
-		diceCount=$(($diceCount + 1))
+		if [ $player == 1 ]
+		then
+			diceCount1=$(($diceCount1 + 1))
+		else
+			diceCount2=$(($diceCount2 + 1))
+		fi	
 		echo "The number on Dice is : "$dice
 	}
-
-function checkOption()
+function checkOptionplayer1()
 	{
 		option=$((1+RANDOM%3))
 		case $option in
-			1)PlayerCurrentPosition=$PlayerCurrentPosition
+			1)PlayerOnePosition=$PlayerOnePosition
 				;;
-			2)PlayerCurrentPosition=$(($PlayerCurrentPosition + $dice))
+			2)PlayerOnePosition=$(($PlayerOnePosition + $dice))
 				;;
-			3)PlayerCurrentPosition=$(( $PlayerCurrentPosition - $dice))
-				if [[ $PlayerCurrentPosition -lt 0 ]]
+			3)PlayerOnePosition=$(( $PlayerOnePosition - $dice))
+				if [[ $PlayerOnePosition -lt 0 ]]
 				then
-					PlayerCurrentPosition=0
+					PlayerOnePosition=0
 				fi
 				;;
 		esac
-		echo "number of time dice was played : $diceCount"
-		echo "player current position is :" $PlayerCurrentPosition 
+		echo $PlayerOnePosition
+	}
+function checkOptionplayer2()
+	{
+		option=$((1+RANDOM%3))
+		case $option in
+			1)PlayerTwoPosition=$PlayerTwoPosition
+				;;
+			2)PlayerTwoPosition=$(($PlayerTwoPosition + $dice))
+				;;
+			3)PlayerTwoPosition=$(( $PlayerTwoPosition - $dice))
+				if [[ $PlayerTwoPosition -lt 0 ]]
+				then
+					PlayerTwoPosition=0
+				fi
+				;;
+		esac
+		echo $PlayerTwoPosition
 	}
 
 function checkWin()
 	{
-		while [[ $PlayerCurrentPosition -lt $WinningPosition ]]
+		while [[ $PlayerOnePosition -lt $WinningPosition && $PlayerTwoPosition -lt $WinningPosition ]]
 		do
+			playerTurn
+
 			rollDice
-			if [[ $(($PlayerCurrentPosition + $dice)) -gt 100 ]]
-			then 
-				$playerCurrentPosition=$PlayerCurrentPosition
+			if [ $player == 1 ]
+ 			then
+				if [[ $(($PlayerOnePosition + $dice)) -gt 100 ]]
+				then 
+					PlayerOnePosition=$PlayerOnePosition
+ 				else
+ 					checkOptionplayer1
+ 				fi
 			else
-				checkOption
+				if [[ $(($PlayerTwoPosition + $dice)) -gt 100 ]]
+				then
+					PlayerTwoPosition=$PlayerTwoPosition
+				else
+					checkOptionplayer2
+				fi
 			fi
-			if [[ $PlayerCurrentPosition == $WinningPosition ]]
+			if [ $PlayerOnePosition == $WinningPosition ]
 			then
-				echo "YOU WIN ....!!!"
+				echo "PLAYER ONE WON ....!!!"
+			elif [ $PlayerTwoPosition == $WinningPosition ]
+			then 
+				echo "PLAYER TWO WON ....!!!"
 			fi
 		done
 	}
