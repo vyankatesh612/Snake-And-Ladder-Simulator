062631a6f81d372b8d45866be240dc5a04ad2aa3
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-11 09:10:20 +0530

    ADDED : player rolls the die

--- snakeAndladder.sh
+++ snakeAndladder.sh
@@ -5,4 +5,9 @@ echo "WelCome to Snake and Ladder Computation"
 #CONSTANT VARIABLE
 PYAYER_POSITION=0
 
-
+function rollDice()
+	{
+		dice=$((1+RANDOM%6))
+		echo "The number on Dice is : "$dice
+	}
+rollDice
